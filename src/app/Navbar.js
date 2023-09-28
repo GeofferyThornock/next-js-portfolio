6653267e60481086f4d836b571eb897222a1bba2
GeofferyThornock <geofferythornock@gmail.com>
2023-09-28 16:47:29 -0500

    inital commit

---
+++ src/app/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <div className="flex text-lg pt-3 pr-10 gap-8">
+            <p>About</p>
+            <p>Projects</p>
+        </div>
+    );
+}
